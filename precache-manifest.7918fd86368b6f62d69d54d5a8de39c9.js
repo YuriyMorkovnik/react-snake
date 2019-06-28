0b1b0033ec911db6fb7d96b9311fc5932c96310b
Yuriy Morkovnik <yuriymork@yandex.ru>
2019-06-28 10:08:52 +0300

    Updates

---
+++ precache-manifest.7918fd86368b6f62d69d54d5a8de39c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4981af7a38199881ccda64cc206d2774",
+    "url": "/react-snake/index.html"
+  },
+  {
+    "revision": "7f8b48bb8a0d68e6e41d",
+    "url": "/react-snake/static/css/main.cb9ed7bd.chunk.css"
+  },
+  {
+    "revision": "9e3b0f83770cc6ea9601",
+    "url": "/react-snake/static/js/2.3cf7d0d9.chunk.js"
+  },
+  {
+    "revision": "7f8b48bb8a0d68e6e41d",
+    "url": "/react-snake/static/js/main.9c6ced73.chunk.js"
+  },
+  {
+    "revision": "2c8dd5b493a3fcff3c72",
+    "url": "/react-snake/static/js/runtime~main.c77650c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-snake/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80f019908a609b41737336cb22291ffa.js"
+  "/react-snake/precache-manifest.7918fd86368b6f62d69d54d5a8de39c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-snake/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
